6bb3eaea27da4da142b5cdd5c272eb372e2a9c96
Bryan Elliott <bryan.elliott@cesium.com>
2024-10-09 10:53:05 -0400

    Changes for cross-domain

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## Demonstration of the SameSite=Strict solution
 
-1. Run `npm start`
-2. Browse to http://localhost:8080
-3. Observe in dev tools that the cookie "authCookie" is set with SameSite=Strict
-3. Open `attack.html` as a local file
-4. Open dev tools and go to the network tab
-5. Click the button
-6. Observe that the POST request does not contain any cookies.
+1. Add `127.0.0.1 client.localhost` and `127.0.0.1 api.localhost` to your hosts file.
+2. Run `npm start`
+3. Browse to http://localhost:8080
+4. Observe in dev tools' network tab that the cookie "authCookie" is set with SameSite=Strict, and that the `makeApiCall` request includes that cookie.
+5. Open `attack.html` as a local file
+6. Open dev tools and go to the network tab
+7. Click the button
+8. Observe that the POST request does not contain any cookies.  The response will have the `set-cookie` header.
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import http from "node:http";
-import { createReadStream } from 'node:fs';
+import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 const clientRoot = new URL("../client", import.meta.url).pathname;
@@ -11,13 +11,45 @@ const formatCookies = (name, value, options) => [
 ].join('; ');
 
 
-const server = http.createServer(function (req, res) {
-  res.setHeader("set-cookie", formatCookies("authCookie", "12345", {
-    HttpOnly: true,
-    SameSite: "Strict",
-  }));
+const server = http.createServer(async (req, res) => {
+  if (req.headers.referer) {
+    res.setHeader("access-control-allow-origin", req.headers.referer.replace(/\/$/, ''));
+    res.setHeader("access-control-allow-credentials", true);
+    res.setHeader("vary", "Origin");
+  }
+
+  if (req.headers.host === 'api.local.host:8080' && req.url === '/authenticate') {
+    res.setHeader("set-cookie", formatCookies("authCookie", "12345", {
+      HttpOnly: true,
+      SameSite: "Strict",
+      Domain: "api.local.host",
+    }));
+    res.writeHead(200);
+    res.write('');
+    res.end();
+    return;
+  }
+  if (req.headers.host === 'api.local.host:8080' && req.url === '/makeApiCall') {
+    const cookies = (req.headers.cookie ?? "").split(';').reduce((o, c) => {
+      const [name, ...value] = c.split('=');
+      o[name.trim()] = value.join('=').trim();
+      return o;
+    }, {});
+    console.log(cookies);
+    if (cookies.authCookie === '12345') {
+      res.writeHead(200);
+      res.write("You're good.");
+    } else {
+      res.writeHead(401);
+      res.write("Go away, hacker.");
+    }
+    res.end();
+    return;
+  }
+  res.setHeader("access-control-allow-credentials", "true");
   res.writeHead(200);
-  const content = createReadStream(join(clientRoot, "index.html")).pipe(res);
+  const content = await readFile(join(clientRoot, "index.html"));
+  res.write(content);
   res.end();
 });
 
